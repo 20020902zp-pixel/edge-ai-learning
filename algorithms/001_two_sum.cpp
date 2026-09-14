0efc03fa53a683ce1a7e69c2a4c326f463e97cfa
zp <20020902zp@gmail.com>
2026-09-14 23:00:15 +0800

    day1: complete two sum solution

--- algorithms/001_two_sum.cpp
+++ algorithms/001_two_sum.cpp
@@ -1,2 +1,24 @@
 //LeetCode 1:Two  sum
 //Day  1 
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int need = target - nums[i];
+
+            if (mp.count(need)) {
+                return {mp[need], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
